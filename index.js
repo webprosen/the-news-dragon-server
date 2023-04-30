a5a98f1f34614a1db5e35ae7c15e09fa4cb62168
Prosen Biswas <webprosen@gmail.com>
2023-05-01 00:44:25 +0600

    staging

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 const categories = require('./data/categories.json');
 const news = require('./data/news.json');
-
 const cors = require('cors');
 
 app.use(cors());
@@ -14,6 +13,25 @@ app.get('/', (req, res)=>{
 
 app.get('/categories', (req, res)=>{
     res.send(categories);
+});
+
+app.get('/categories/:id', (req, res)=>{
+    const id = req.params.id;
+    if(id == 0){
+        res.send(news);
+    } else {
+        const categoryNews = news.filter(item => item.category_id === id);
+        res.send(categoryNews);
+    }
+});
+
+app.get('/news', (req, res)=>{
+    res.send(news);
+});
+
+app.get('/news/:id', (req, res)=>{
+    const singleNews = news.find(item => item._id === req.params.id);
+    res.send(singleNews);
 });
 
 app.listen(port, ()=>{
